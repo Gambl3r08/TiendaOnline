a26967afcbc6894bceb5ad51cf534f4cad4e15df
Gambl3r <robertojoselozada@gmail.com>
2020-04-12 22:40:00 -0500

    x

--- GestionPedidos/views.py
+++ GestionPedidos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# GIT URL: https://github.com/Gambl3r08/TiendaOnline.git
+
--- tiendaOnline/settings.py
+++ tiendaOnline/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tiendaonline',
+        'USER': 'postgres',
+        'PASSWORD': 'r3l0j.py',
+        'HOST': 'localhost',
+        'DATABASE_PORT': '5432',
     }
 }
 
